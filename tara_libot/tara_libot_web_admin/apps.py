3c6648bbb6e939613c165063388be8a77d58e6fb
Ronuel Joshua B. Reyes <ronuelreyes@gmail.com>
2023-05-17 20:14:39 +0800

    17/05/2023 Added: 1. Project Structure

---
+++ tara_libot/tara_libot_web_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaraLibotWebAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tara_libot_web_admin'
---
+++ tara_libot/tara_libot_web_client/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaraLibotWebClientConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tara_libot_web_client'
