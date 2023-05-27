4f24c64dda5e88e9f57f0a9d25291990b6e1adfb
JohnCarlSalao <salaojohn4@gmail.com>
2023-05-27 12:12:18 +0800

    27/05/2023 1.Added OSMGeoAdmin @admin.py

--- tara_libot_web_client/admin.py
+++ tara_libot_web_client/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from tara_libot_web_client.models.models import *
+from django.contrib.gis.admin import OSMGeoAdmin
 
 # Register your models here.
 admin.site.register(Foods)
-admin.site.register(Business)
+
+
 admin.site.register(Comments)
 admin.site.register(Account)
+admin.site.register(Business)
+class ShopAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
