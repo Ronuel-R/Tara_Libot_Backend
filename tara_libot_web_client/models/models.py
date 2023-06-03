10e66d4fee5f69c3a0704669aeccd2878d223568
johncarlsalao <salaojohn4@gmail.com>
2023-06-03 13:36:58 +0800

    03/06/2023 1.) Change models id to AutoField 2.) added MAPBOX_ACCESS_TOKEN at settings.py 3.) Added locationField into models.py

--- tara_libot_web_client/models/models.py
+++ tara_libot_web_client/models/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from tara_libot_web_client.storage import OverwriteStorage
+from mapbox_location_field.models import LocationField
 
 def rename_profile(instance, filename):
     extension = filename.split('.')[-1]
@@ -34,7 +35,7 @@ class Account(models.Model):
         return self.user.first_name
 
 class Business(models.Model):
-    id = models.CharField(max_length=5, primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=256)
     city = models.CharField(max_length=256)
@@ -46,11 +47,13 @@ class Business(models.Model):
     rating = models.FloatField(null=True)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
+    location= LocationField()
     
     def __str__(self):
         return self.name
 
 class Comments(models.Model):
+    id = models.AutoField(primary_key=True)
     content = models.CharField(max_length= 2000)
     business = models.ForeignKey(Business,on_delete=models.CASCADE)
     rating = models.FloatField()
@@ -76,11 +79,23 @@ class Foods(models.Model):
         (PASALUBONG, 'Pasalubong'),
     ]
 
-    id = models.CharField(max_length=5, primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     price = models.FloatField()
     business = models.ForeignKey(Business, on_delete=models.CASCADE)
     food_photo = models.ImageField(null=True)
+
+class FoodComments(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length= 2000)
+    food = models.ForeignKey(Foods,on_delete=models.CASCADE)
+    rating = models.FloatField()
+    created_by= models.ForeignKey(Account, on_delete=models.CASCADE )
+    created_at = models.DateField(timezone.now,null=False,editable=False)
+    likes= models.IntegerField()
+    #reply_comment=models.ForeignKey(Comments, on_delete= models.CASCADE )
+    def __str__(self):
+        return self.content
     
     
